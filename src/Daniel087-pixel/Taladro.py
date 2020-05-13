d64a6945062147c5befc67531f623fae2e1657f7
Daniel087-pixel <mpdaniel37@gmail.com>
2020-05-13 14:57:03 -0500

    taldro con tipos

---
+++ src/Daniel087-pixel/Taladro.py
@@ -0,0 +1,7 @@
+class Taladro():
+    def __init__(self,velocidades,tipo)
+    self.velocidades = velocidades
+    self.tipo = tipo
+
+    def imprimirinfo(self):
+        print(f"Taladro: {self.velocidades} Tipo: {self.tipo}")
