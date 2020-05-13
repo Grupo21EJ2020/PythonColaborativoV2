9aa1b261538f1bb6039048d7dd61d88c3887b2ac
EdgarZamoraa <zamorasaucedoedgar@gmail.com>
2020-05-13 15:29:40 -0500

    Se agrego en main las clases auto y zapato

--- src/EdgarZamoraa/main.py
+++ src/EdgarZamoraa/main.py
@@ -1,5 +1,12 @@
 from clasess import *
 
+print("")
 marcador = Marcador("sharpie",199,"Negro")
-
 marcador.imprimirinfo()
+print("")
+zapato = Zapato("Azul con negro",2699,"NIKE")
+zapato.imprimirinfo()
+print("")
+auto = Automovil("negro",8,"Cadillac Brougham Fleetwood")
+auto.imprimirinfo()
+print("")
