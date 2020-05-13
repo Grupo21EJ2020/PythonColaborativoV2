3f097e935cd2511b7584b088ab5d368a80036d7b
Edith Paola Vidales Velázquez <edith_tigresa10@hotmail.com>
2020-05-13 15:09:26 -0500

    Se verificó la Clase2

--- src/EdithVidales/Main.py
+++ src/EdithVidales/Main.py
@@ -1,7 +1,10 @@
 from Clase1 import FavoritoColor
+from Clase2 import LugarPreferido
 
 colorfav = FavoritoColor("Edith", "Morado")
 
 colorfav.ImprimirInfo()
 
+lugpref = LugarPreferido("Mi casa", 10)
 
+lugpref.ImprimirInfo()
