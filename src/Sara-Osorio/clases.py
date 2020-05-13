83bdc09d151c0663f9626c7150cf1b61264f05e9
Sara-Osorio <62372559+Sara-Osorio@users.noreply.github.com>
2020-05-13 17:58:03 -0500

    Add mis 3 clases e Implementacion de imprimirinfo

---
+++ src/Sara-Osorio/clases.py
@@ -0,0 +1,25 @@
+class FlorFav():
+     def __init__(self,nombre,color):
+         self.nombre = nombre
+         self.color = color
+
+     def imprimirinfo(self):
+         print (f"Nombre: {self,nombre} Color: {self.color}")
+
+class Computadora():
+     def __init(self,marca,precio):
+         self.marca = marca
+         self.precio = precio
+
+     def imprimirinfo(self):
+         print (f"Computadora: {self,computadora} Precio: {self.precio}")
+
+ class Shampoo():
+     def __init(self,marca,precio):
+         self.marca = marca
+         self.precio = precio
+
+     def imprimirinfo(self):
+         print (f"Marca: {self,marca} Precio: {self.precio}")        
+
+        
