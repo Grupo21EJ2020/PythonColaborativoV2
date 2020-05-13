01f850d4e71d8f5f51ac99164d02e5db5a53a51b
Jonathan Sauceda <jonathansauceda744@gmail.com>
2020-05-13 17:00:15 -0500

    Agregue codigo a main.py

---
+++ src/JONATHANSAUCEDA/main.py
@@ -0,0 +1,10 @@
+from clases import * 
+
+LigaFut = LigaDeFut('LigaMx')
+LigaFut.imprimirinfo()
+
+Equipos = Equipo('TigresUANL')
+Equipos.imprimirinfo()
+
+Campeonatos = Titulos(7)
+Campeonatos.imprimirinfo()
