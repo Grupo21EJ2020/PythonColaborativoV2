c49abb4bf522a2abee0fd25ca43c5ea84eb25636
Alejandro Rodriguez <alex.rdz.chavi@gmail.com>
2020-05-12 23:21:30 -0500

    Agregue mis clases

---
+++ src/AlejandroRCH/Clases.py
@@ -0,0 +1,25 @@
+class Alumno():
+    def __init__(self,Nombre,Edad):
+        self.Nombre = Nombre
+        self.Edad = Edad
+    def imprimirinfo(self):
+        print(f"Nombre: {self.Nombre} Edad: {self.Edad} ")  
+class Maestro():
+    def __init__(self,Nombre,Edad):
+        self.Nombre = Nombre
+        self.Edad = Edad
+    def imprimirinfo(self,):
+        print(f"Nombre: {self.Nombre} Edad: {self.Edad} ")   
+class OtroAlumno():
+    def __init__(self,Nombre,Edad):
+        self.Nombre = Nombre
+        self.Edad = Edad
+    def imprimirinfo(self):
+        print(f"Nombre: {self.Nombre} Edad: {self.Edad} ")   
+
+
+       
+
+    
+
+
