c09d4b1e38a7f62e966255aa10f5dd94d8a2130c
SammyDeAztlan <silver.palacios@gmail.com>
2020-05-13 14:55:33 -0500

    se agregaron 2 clases

--- src/sammy.py/claseschidas.py
+++ src/sammy.py/claseschidas.py
@@ -1,4 +1,26 @@
-class juego():
-    def _init_(self,titulo,precio):
+class Videojuego():
+    def _init_(self,titulo,precio,consola):
         self.titulo = titulo
         self.precio = precio
+        self.consola = consola
+    
+    def printdatos(self):
+        print(f"Titulo: {self.titulo} Precio: {self.precio} Consola: {self.consola}")
+
+class Pelicula():
+    def _init_(self,titulo,director,año):
+        self.titulo = titulo
+        self.director = precio
+        self.año = consola
+    
+    def printdatos(self):
+        print(f"Titulo: {self.titulo} Director: {self.director} Año: {self.año}")
+
+class Coche():
+    def _init_(self,modelo,compañia,color):
+        self.titulo = modelo
+        self.precio = compañia
+        self.consola = color
+    
+    def printdatos(self):
+        print(f"Modelo: {self.modelo} Compañia: {self.compañia} Color:  {self.color}")
