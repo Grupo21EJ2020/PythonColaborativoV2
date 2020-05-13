8bed5b0f7867d2c9ed14b5b602320e262995393a
denilson <islasdeni@gmail.com>
2020-05-13 14:39:25 -0500

    Agregue Clase Iphone

--- src/Denilsonislas/Clases.py
+++ src/Denilsonislas/Clases.py
@@ -5,3 +5,20 @@ class XboxOne():
     
     def imprimirinfo(self):
         print(f"Version: {self.version} Color: {self.color}")
+
+
+class Celular():
+    def __init__(self,marca,precio):
+        self.marca = marca
+        self.precio = precio
+    
+    def imprimirinfo(self):
+        print(f"Marca: {self.marca} Precio: {self.precio}")
+
+class Iphone():
+    def __init__(self,version,capacidad):
+        self.version = version
+        self.capacidad = capacidad
+    
+    def imprimirinfo(self):
+        print(f"Version: {self.version} Capacidad: {self.capacidad}")
