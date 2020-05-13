1c4d81a612be34c0505280a6cfd1286f9c70c29b
DanielCruz2906 <danielcruz75357@gmail.com>
2020-05-13 15:02:44 -0500

    termine las clases

---
+++ src/DanielCruz2906/test_clases.py
@@ -0,0 +1,10 @@
+from clases import *
+
+Auto = Automovil("Toyota",2300,2004)
+Auto.imprimirinfo()
+
+Compu = Computadora("intel","nvidia","8gb")
+Compu.imprimirinfo()
+
+Tele = Television("30'","50000","LG")
+Tele.imprimirinfo()
