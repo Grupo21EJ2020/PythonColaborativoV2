60221144f4c2265c2491ef1d3db593eb9d8f369e
Israel Lopez Cardona <isrrael0919@gmai.com>
2020-05-13 16:21:01 -0500

    Segunda clase

--- src/Israel391/clases.py
+++ src/Israel391/clases.py
@@ -5,3 +5,11 @@ class Libro():
         self.editorial=editorial
     def imprimirInfo(self):
         print(f'Autor: {self.autor}, numpaginas: {self.numpaginas}, editorial: {self.editorial}')
+
+class Bicicleta():
+    def __init__(self,color,modelo,categoria):
+        self.color=color
+        self.modelo=modelo
+        self.categoria=categoria
+    def imprimirInfo(self):
+        print(f'Color: {self.color}, Modelo: {self.modelo}, Categoria: {self.categoria}')
