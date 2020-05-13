51b33ecb6c899e0e38dc54e6e48be504673f79b8
Omar Gutierrez Garcia <omarg.garcia.1999@gmail.com>
2020-05-13 16:12:23 -0500

    Agregue codigo de prueba de las clases

---
+++ src/kozel99/clases_prueba.py
@@ -0,0 +1,10 @@
+from clases import *
+
+miComputadora = Computadora('HP',1700)
+miComputadora.imprimirInfo()
+
+miCelular = Celular('Motorola',1500,'Negro')
+miCelular.imprimirInfo()
+
+miImpresora = Impresora(1800,'Blanca','48 hojas/minuto')
+miImpresora.imprimirInfo()
