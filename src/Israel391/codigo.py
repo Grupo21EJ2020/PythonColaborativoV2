6527c30d571c2b4d22de5822a58611d00690e8a0
Israel Lopez Cardona <isrrael0919@gmai.com>
2020-05-13 16:35:17 -0500

    Calando clases

---
+++ src/Israel391/codigo.py
@@ -0,0 +1,10 @@
+from clases import *
+
+bicituneada = Bicicleta('Azul',2001,'Profesional')
+bicituneada.imprimirInfo()
+
+librito=Libro('Edgar Allan Poe',200,'Los pitufos')
+librito.imprimirInfo()
+
+perfumazo=Perfume('CR7',2020)
+perfumazo.imprimirInfo()
