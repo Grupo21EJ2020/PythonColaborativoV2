2a9bf0f4ae94217e42f242f12272427e3f8ec4bd
Pablo Emmanuel Noh Torres <emmanuelkxl@gmail.com>
2020-05-13 21:28:04 -0500

    Hice cambios en la clase

--- src/Emmanuelk3/Class.py
+++ src/Emmanuelk3/Class.py
@@ -1,8 +1,8 @@
 class Telefono():
-    def __init__(self,Marca,Precio,Color):
+    def __init__(self,Marca,Precio):
         self.Marca = Marca
         self.Precio = Precio
-        self.Color = Color
+     
         
     def imprimirinfo(self):
-        print(f"Marca: [self.marca] Precio: [Self.Precio]")
+        print(f"Marca: {self.Marca} Precio: {self.Precio}")
