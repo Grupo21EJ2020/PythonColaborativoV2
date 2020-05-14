cf949bd4e42d1e40d010400de6a707fa6ec39b42
Pablo Emmanuel Noh Torres <emmanuelkxl@gmail.com>
2020-05-13 21:59:11 -0500

    Add 3 clases más

--- src/Emmanuelk3/Class.py
+++ src/Emmanuelk3/Class.py
@@ -6,3 +6,33 @@ class Telefono():
         
     def imprimirinfo(self):
         print(f"Marca: {self.Marca} Precio: {self.Precio}")
+
+
+
+class Bicicletas():
+    def __init__(self,Color,Modelo):
+        self.Color = Color
+        self.Modelo = Modelo 
+
+    def imprimirinfo(self):
+        print(f"Color: {self.Color} Modelo: {self.Modelo}")
+
+
+
+class Tenis():
+    def __init__(self,Tipo,Talla):
+        self.Tipo = Tipo
+        self.Talla = Talla
+    
+    def imprimirinfo(self):
+        print(f"Tipo: {self.Tipo} Talla: {self.Talla}")
+
+
+class Mouse():
+    def __init__(self,Tamaño,Numerodevelocidades):
+        self.Tamaño = Tamaño
+        self.Numerodevelocidades = Numerodevelocidades
+
+  def imprimirinfo(self):
+      print(f"Tamaño: {self.Tamaño} Numerodevelocidades: {self.Numerodevelocidades})
+      
