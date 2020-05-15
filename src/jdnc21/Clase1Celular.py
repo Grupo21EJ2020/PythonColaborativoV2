acc4ca471e523e430de61812ecd09d1f52ec3798
Juan De Dios Nacianceno <hackmasterpro1000@gmail.com>
2020-05-14 21:10:57 -0500

    Se cambio el nombre de la clase

---
+++ src/jdnc21/Clase1Celular.py
@@ -0,0 +1,6 @@
+class Celular():
+    def __init__(self, marca,nombre,color,precio)
+        self.marca = marca
+        self.nombre = nombre 
+        self.color = color 
+        self.precio = precio
