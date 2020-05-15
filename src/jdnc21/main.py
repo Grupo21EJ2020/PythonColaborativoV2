113772997fd6ec2806c98fe012f30b5a3bd41310
Juan De Dios Nacianceno <hackmasterpro1000@gmail.com>
2020-05-15 00:02:02 -0500

    Se agrego codigo de imprimirinfo a la clase 2 y 3

--- src/jdnc21/main.py
+++ src/jdnc21/main.py
@@ -1,6 +1,23 @@
+# Clase 1 
 from Clase1Celular import Celular
 
-CelularSony = Celular("SONY","Walkman A55","Negro",5559)
+print ("TELEFONO MOVIL")
+CelularSony = Celular("SONY","Walkman A55","Negro","$ 5,559\n")
 
 CelularSony.imprimirinfo()
 
+# Clase 2 
+from Clase2Instrumento import Instrumento
+
+print ("INSTRUMENTO")
+Teclado = Instrumento("Casio","CTK-720","3H76JEU8JN2020",3100,"Electronico\n")
+
+Teclado.imprimirinfo()
+
+# Clase 3
+from Clase3Computadora import Computadora
+
+print ("ORDENADOR")
+Ordenador = Computadora("Acer","Predator Orion 9000 Desktop Gaming Computer","HFU48JTRKRJ84",12,"$ 159,554.22")
+
+Ordenador.imprimirinfo()
