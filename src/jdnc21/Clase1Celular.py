80bec8dce04271eea3cc8bb84a946b23e08dddd0
Juan De Dios Nacianceno <hackmasterpro1000@gmail.com>
2020-05-14 23:55:26 -0500

    Se realizo un ajuste en Clase1Celular

--- src/jdnc21/Clase1Celular.py
+++ src/jdnc21/Clase1Celular.py
@@ -1,5 +1,5 @@
 class Celular():
-    def __init__(self, marca,nombre,color,precio)
+    def __init__(self, marca, nombre, color, precio):
         self.marca = marca
         self.nombre = nombre 
         self.color = color 
--- src/jdnc21/Clase3Computadora.py
+++ src/jdnc21/Clase3Computadora.py
@@ -1,11 +1,12 @@
 class Computadora():
-    def __init__(self, marca,nombre,numserie,nummodelo,precio)
+    def __init__(self, marca, nombre, numserie, nummodelo, precio):
         self.marca = marca
         self.nombre = nombre 
         self.numserie = numserie 
         self.nummodelo = nummodelo 
         self.precio = precio
+        self.categoria = categoria
 
     def imprimirinfo(self):
-        print(f"Marca: {self.marca} Nombre: {self.nombre} NumSerie: {self.numserie} NumModelo: {self.nummodelo} Precio {self.precio}")
+        print(f"Marca: {self.marca} Nombre: {self.nombre} NumSerie: {self.numserie} NumModelo: {self.nummodelo} Precio {self.precio} Categoria: {self.categoria}")
          
