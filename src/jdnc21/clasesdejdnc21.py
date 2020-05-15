30ea680776bad100815d35d99608be65c030f15e
Juan De Dios Nacianceno <hackmasterpro1000@gmail.com>
2020-05-14 21:18:01 -0500

    Se borro una clase

--- src/jdnc21/clasesdejdnc21.py
+++
@@ -1,6 +0,0 @@
-class Celular():
-    def __init__(self, marca,nombre,color,precio)
-        self.marca = marca
-        self.nombre = nombre 
-        self.color = color 
-        self.precio = precio
