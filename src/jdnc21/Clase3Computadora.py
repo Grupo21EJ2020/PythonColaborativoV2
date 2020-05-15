1ac9a75b85f2bdf82da5b907e02f16166497bdc3
Juan De Dios Nacianceno <hackmasterpro1000@gmail.com>
2020-05-14 21:45:50 -0500

    Se añadio la informacion a Clase3Computadora

---
+++ src/jdnc21/Clase3Computadora.py
@@ -0,0 +1,8 @@
+class Computadora():
+    def __init__(self, marca,nombre,numserie,nummodelo,precio)
+        self.marca = marca
+        self.nombre = nombre 
+        self.numserie = numserie 
+        self.nummodelo = nummodelo 
+        self.precio = precio
+         
