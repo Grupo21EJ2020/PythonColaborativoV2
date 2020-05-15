691d93918543fafcea61c6ffd88dd9238977bef8
Juan De Dios Nacianceno <hackmasterpro1000@gmail.com>
2020-05-14 21:25:08 -0500

    Se agrego la clase 2 de intrumento

---
+++ src/jdnc21/Clase2Instrumento.py
@@ -0,0 +1,7 @@
+class Instrumento():
+    def __init__(self, marca,nombre,idproducto,precio,categoria)
+        self.marca = marca
+        self.nombre = nombre 
+        self.iproducto = idproducto  
+        self.precio = precio
+        self.precio = categoria
