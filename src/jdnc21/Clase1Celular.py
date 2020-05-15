b00fec09e46a85388b56530d3743a513e918f31c
Juan De Dios Nacianceno <hackmasterpro1000@gmail.com>
2020-05-14 22:01:20 -0500

    Se agrego codigo para imprimir informacion Clase1

--- src/jdnc21/Clase1Celular.py
+++ src/jdnc21/Clase1Celular.py
@@ -4,3 +4,6 @@ class Celular():
         self.nombre = nombre 
         self.color = color 
         self.precio = precio
+
+    def imprimirinfo(self):
+        print(f"Marca: {self.marca} Nombre: {self.nombre} Color: {self.color} Precio: {self.precio}")
