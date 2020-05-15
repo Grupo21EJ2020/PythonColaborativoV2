348dd37f813d67f913e59e30e391d82480f57a9e
Juan De Dios Nacianceno <hackmasterpro1000@gmail.com>
2020-05-14 21:29:18 -0500

    Se añadio una letra faltante

--- src/jdnc21/Clase2Instrumento.py
+++ src/jdnc21/Clase2Instrumento.py
@@ -2,6 +2,6 @@ class Instrumento():
     def __init__(self, marca,nombre,idproducto,precio,categoria)
         self.marca = marca
         self.nombre = nombre 
-        self.iproducto = idproducto  
+        self.idproducto = idproducto  
         self.precio = precio
         self.categoria = categoria
