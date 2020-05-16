0e816c8672181ebdea5daa2d0c2500956f440694
Your Name <you@example.com>
2020-05-16 17:12:04 -0500

    imprimir info

--- src/Alejandro01598/clases.py
+++ src/Alejandro01598/clases.py
@@ -2,3 +2,6 @@ class Auto():
     def _init_(self,Modelo,Marca):
         self.Modelo = Modelo   
         self.Marca = Marca
+
+      def imprimirinfo(self):
+          print(f"Modelo:" {self.Modelo} Marca: {self.Marca}")  
