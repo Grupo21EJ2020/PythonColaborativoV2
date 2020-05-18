d86ea9c5981dcad2411cdf2f4c813a87273d8ae5
Luisa Jasso <Luisajasso2002outlook.com>
2020-05-18 00:02:14 -0500

    Clasesluisa

---
+++ src/luisajasso/ClasesluisaPrueba.py
@@ -0,0 +1,12 @@
+from clases import *
+
+computadora1 = Computadoras("HP, 10500, negro")
+
+computadora1.ImprimirInfo()
+
+Materia1 = Materias("Historia,10,21")
+Materia1.ImprimirInfo()
+
+Lentes1 = Lentes("Modelo1,1500,2.5")
+Lentes1.ImprimirInfo()
+
