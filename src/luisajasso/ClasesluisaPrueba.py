446520cd1cbe2eeb11521ff604d9588790628dee
Luisa Jasso <Luisajasso2002outlook.com>
2020-05-18 00:47:29 -0500

    Se hicieron las pruebas

--- src/luisajasso/ClasesluisaPrueba.py
+++ src/luisajasso/ClasesluisaPrueba.py
@@ -1,12 +1,12 @@
 from clases import *
 
-computadora1 = Computadoras("HP, 10500, negro")
+Computadora1 = Computadoras("HP",1500,"negro")
 
-computadora1.ImprimirInfo()
+Computadora1.ImprimirInfo()
 
-Materia1 = Materias("Historia,10,21")
+Materia1 = Materias("Historia",10,21)
 Materia1.ImprimirInfo()
 
-Lentes1 = Lentes("Modelo1,1500,2.5")
+Lentes1 = Lentes("Modelo1",1500,2.5)
 Lentes1.ImprimirInfo()
 
