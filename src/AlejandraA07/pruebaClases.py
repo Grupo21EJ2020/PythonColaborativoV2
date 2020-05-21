d928af3adda3835b90e89b71f90c054fbea9bc6b
Alejandra Anahi Correa Aldape <alecorrea0752@gmail.com>
2020-05-21 16:26:40 -0500

    add codigo a pruebaClases.py

---
+++ src/AlejandraA07/pruebaClases.py
@@ -0,0 +1,15 @@
+from clases import *
+
+libroEspañol = Libro("Español", "Jesus Hernandez", "alfaomega")
+
+libroEspañol.imprimirinfo()
+
+
+Alumno12 = Alumno (1869699,"Alejandra Correa", "LTI")
+
+Alumno12.imprimirinfo()
+
+
+Pelicula1 = Pelicula ("Iron Man", "accion","126 min")
+
+Pelicula1.imprimirinfo()
