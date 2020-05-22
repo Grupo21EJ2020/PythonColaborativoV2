2c07364e53d3889640e7d7fe6ec141841f8445d8
danna-borda02 <61365959+danna-borda02@users.noreply.github.com>
2020-05-22 17:36:45 -0500

    Correcion de pruebas de las clases

--- src/danna-borda02/testclases.py
+++ src/danna-borda02/testclases.py
@@ -1,10 +1,14 @@
-from misclases import Libretas, zapatos, libro
+from misclases import Libretas
+from misclases import zapatos
+from misclases import libro
 print("Caracteristicas de libretas: ")
 Libretas = Libretas(100, "resorte")
 Libretas.imprimirinfo()
+
 print("Caracteristicas de zapatos: ")
- zapatos= zapatos(23 , "negros")
+zapatos= zapatos(23 , "negros")
 zapatos.imprimirinfo()
+
 print("Datos de la libro: ")
 libro = libro("Grande", "Programacion" )
 libro.imprimirinfo()
