96deaa88c7ae518fffec115a01edd3bd3d90af94
danna-borda02 <61365959+danna-borda02@users.noreply.github.com>
2020-05-22 16:46:55 -0500

    imprimir info

--- src/danna-borda02/misclases.py
+++ src/danna-borda02/misclases.py
@@ -2,3 +2,6 @@ class Libretas():
     def __init__ ( self, hojas, tipo ):
         self.hojas = hojas
         self.tipo = tipo
+
+    def imprimirinfo(self):
+        print(f"Hojas: {self.hojas} tipo: {self.tipo}")
