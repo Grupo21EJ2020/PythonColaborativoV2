eb7dcfeffbb94b3a15b62985c45b13a02c152b59
EEAM-ElPolloSad <edgar.arriagama@uanl.edu.mx>
2020-05-22 19:12:29 -0500

    pruebas.py

---
+++ src/EEAM-ElPolloSad/pruebas.py
@@ -0,0 +1,25 @@
+from clases import  Laptop, Maestros,  Serie, Tablet, Ropa, Beisbol 
+
+Computadora1 = Laptop("Azul","LG","Windows")
+Computadora1.imprimirinfo
+imprimir ("")
+
+Maestros1 = Maestros(1879463,"Jhon Cena","Ingeniero")
+Maestros1.imprimirinfo
+imprimir ("")
+
+Serie1 = Serie("The Mandalorian","Aventura","20min")
+Serie1.imprimirinfo
+imprimir ("")
+
+Tablet1 = Tablet("LG",4000,"Clase Media-Baja")
+Tablet1.imprimirinfo
+imprimir ("")
+
+Ropa1 = Ropa("Nike",4500,28)
+Ropa1.imprimirinfo
+imprimir ("")
+
+Equipo1 = EquipoBeisbol("Sultanes",174,"Felix Perez")
+EquipoBeisbol1.imprimirinfo
+imprimir ("")
