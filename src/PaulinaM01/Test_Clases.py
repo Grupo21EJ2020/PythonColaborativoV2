0ecbca46b71bda94f9b97bf92c3b106af06456cb
Paulina Mendoza <Michelle_1068@outlook.es>
2020-05-25 10:26:48 -0500

    Se modifico Test_Clases.py

---
+++ src/PaulinaM01/Test_Clases.py
@@ -0,0 +1,10 @@
+from clases import *
+
+NuevaBocina=Bocina("BOSE","Portatil", 25000)
+NuevaBocina.imprimirinfo()
+
+NuevaConsola=Consola_Videojuegos("Xbox",4000 )
+NuevaConsola.imprimirinfo()
+
+NuevosAudifonos=Audifonos("Sony", "Blancos", 400)
+NuevosAudifonos.imprimirinfo()
