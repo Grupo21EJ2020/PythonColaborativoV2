d4ce06b1f4504e018b47d0455032da9c397ff2e7
Alejandro01598 <alejajosez7@gmail.com>
2020-05-25 22:51:55 -0500

    importe mis clases

---
+++ src/Alejandro01598/test_clases.py
@@ -0,0 +1,10 @@
+from clases import *
+
+Auto1 = Auto(2020,"Ford")
+Auto1.imprimirinfo()
+
+Alumno1 = Alumno("LTI",2)
+Alumno1.imprimirinfo()
+
+Pelicula1 = Pelicula("It","Terror")
+Pelicula1.imprimirinfo()
