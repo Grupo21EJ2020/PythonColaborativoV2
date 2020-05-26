2ce1a827b43f311ccc2e85bfb7eebb51f6aecfb8
MAXIMILIANO <maxalvarez18@hotmail.com>
2020-05-25 23:05:56 -0500

    Creando otra clase

---
+++ src/MaximilianoAlvarez/Creando3Clase.py
@@ -0,0 +1,6 @@
+class zapatos
+    def_init_(self,talla,color):
+        self.talla = talla
+
+    def imprimirinfo(self)
+        print(f"talla: {self.talla} color: { self.color})
---
+++ src/MaximilianoAlvarez/CreandoOtraClase.py
@@ -0,0 +1,4 @@
+class Libro():
+    def_init_(self,tamaño,materia):
+        self.tamaño = tamaño
+        sel.materia = materia
