bcf9ce9cb228907ea75a283f4b913ffc962f08c8
Alejandro01598 <alejajosez7@gmail.com>
2020-05-25 22:50:56 -0500

    agregue mis clases

---
+++ src/Alejandro01598/clases.py
@@ -0,0 +1,24 @@
+class Auto():
+    def _init_(self,modelo,marca):
+        self.modelo = modelo   
+        self.marca = marca
+
+    def imprimirinfo(self):
+        print(f"Modelo: {self.modelo} Marca: {self.marca}")  
+          
+class Alumno():
+    def _init_(self,carrera,semestre):
+        self.carrera=carrera
+        self.semestre=semestre
+        
+    def imprimirinfo(self):
+        print(f"Carrera : {self.carrera} Semestre:{self.semestre}")
+
+class Pelicula():
+    def _init_(self,titulo,genero):
+        self.titulo=titulo
+        self.genero=genero
+
+    def imprimirinfo(self):
+        print(f"Titulo: {self.titulo} Genero:{self.genero}")
+
