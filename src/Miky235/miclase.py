842490686433f91e7f01ff5c14062bccfbfc04e9
Miky235 <milo_rayo@hotmail.com>
2020-05-26 05:16:40 -0500

    Agregue mis 3 clases

---
+++ src/Miky235/miclase.py
@@ -0,0 +1,26 @@
+class Celular():
+    def __init__(self, marca,precio,color):
+        self.marca = marca
+        self.precio = precio
+        self.color = color
+    def ImprimirInfo(self):
+        print(f"El celular es de la marca {self.marca} del precio {self.precio} del color {self.color}")
+
+
+
+class Perfume():
+    def__init__(self,marca,edicion):
+        self.marca=marca
+        self.edicion=edicion
+    def ImprimirInfo(self):
+        print(f"El perfume es de la marca {self.marca} y edicion {self.edicion} ")
+
+
+
+class Jersey():
+    def __init__ (self,modelo, precio, graduacion):
+        self.modelo = equipo
+        self.precio = precio
+        self.graduacion = color
+    def ImprimirInfo(self):
+        print(f"El jersey es del equipo {self.equipo} con el precio{self.precio} y con el color {self.color}")
