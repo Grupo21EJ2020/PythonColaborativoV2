c4f8aa7f53827f7543412eb92325c2af1377b082
Miky235 <milo_rayo@hotmail.com>
2020-05-26 05:18:13 -0500

    Agregue los ejemplos para utilizar mi clase

---
+++ src/Miky235/pruebaclase.py
@@ -0,0 +1,11 @@
+from miclase import *
+
+Celular1 = Computadoras("Samsung",1500,"negro")
+
+Computadora1.ImprimirInfo()
+
+Perfume1 = Perfume("Gucci","ultima")
+Perfume1.ImprimirInfo()
+
+Jersey1 = Jersey("Broncos",1500,"Blanco")
+Jersey1.ImprimirInfo()
