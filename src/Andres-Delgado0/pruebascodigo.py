235b8dd18dc9c31f879078dfe0b7d827d4d2a97e
Andres Eduardo Delgado Galván <andreshog@gmail.com>
2020-05-27 02:36:38 -0500

    Se añadio archivo para las  pruebas con un nuevo nombre

---
+++ src/Andres-Delgado0/pruebascodigo.py
@@ -0,0 +1,14 @@
+from clases import *
+
+Humano1 = Humano("Mestizo", "Carlos", 170, 20, "Colombia", "Catolicismo")
+
+Humano1.imprimirinfo()
+
+M1 = Materia("Antropología", "Ciencia que estudia los cambios y progresos de la humanidad", "Humanidades", "Facultad de Filosofía", "Jorge Romero") 
+
+M1.imprimirinfo()
+
+Personaje1 = Personaje("Harry Osborn", "Franquicia de Spider-man", "Secundario", "Estadounidense")
+
+Personaje1.imprimirinfo() 
+
