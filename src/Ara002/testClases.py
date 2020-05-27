58ec677b12992a3ea4cb47dd9beac64d925f55cf
Aranza Jaqueline Gonzalez Castillo <aranza.gonzalezcl@uanl.edu.mx>
2020-05-26 19:27:58 -0500

    Agregue tesClases

---
+++ src/Ara002/testClases.py
@@ -0,0 +1,10 @@
+from Clases import *
+
+LaptopC = Laptop ("Dell","Negra") 
+LaptopC.imprimirinfo()
+
+CelularS = Celular("Samsung",4000) 
+CelularS.imprimirinfo()
+
+LibretaS = Libreta("Resorte","12") 
+LibretaS.imprimirinfo()
