3e85695e7598a460585afda1a77d029424b811a0
Daniel Alejandro Siller Garcia <danny.999@live.com.mx>
2020-05-27 21:34:38 -0500

    Agregue mi segunda clase llamada VideoJuegos a MisClases.py

--- src/29Daniel29Siller/MisClases.py
+++ src/29Daniel29Siller/MisClases.py
@@ -6,3 +6,13 @@ class Ropa():
 
     def imprimirInfo(self):
         print( f"Marca: {self.marca}\n Precio: {self.precio}\n Talla: {self.talla}" )
+
+#CLASE NUMERO 2---
+class Videojuegos():
+    def __init__(self, genero, tamaño, duracion):
+        self.genero = genero
+        self.tamaño = tamaño
+        self.duracion = duracion
+
+    def imprimirInfo(self):
+        print( f"Genero: {self.genero}\n Tamaño: {self.tamaño}\n Duracion: {self.duracion}" )
